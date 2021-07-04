a7a82a80563100968b6265d7c953dbb86501657e
Florian Niederhuber <f.niederhuber@nwork.at>
2021-07-04 17:41:25 +0200

    Configured color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      current: "currentColor",
+      red: "#FF595E",
+      orange: "#FFCA3A",
+      green: "#8AC926",
+      blue: "#1982C4",
+      purple: "#6A4C93",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
